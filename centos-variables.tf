423888c61915bff2956a852b61c16e78bf621dcb
Guillermo Musumeci <guillermo-musumeci@users.noreply.github.com>
2022-02-06 20:38:33 +0100

    Initial commit

---
+++ centos-variables.tf
@@ -0,0 +1,40 @@
+######################
+## Ubuntu Variables ##
+######################
+
+variable "linux_vm_image_publisher" {
+  type        = string
+  description = "Virtual machine source image publisher"
+  default     = "OpenLogic"
+}
+
+variable "linux_vm_image_offer" {
+  type        = string
+  description = "Virtual machine source image offer"
+  default     = "UbuntuServer"
+}
+
+variable "centos_7_sku" {
+  type        = string
+  description = "SKU for latest CentOS 8 "
+  default     = "7_9"
+}
+
+variable "centos_7_gen2_sku" {
+  type        = string
+  description = "SKU for latest CentOS 8 Gen2"
+  default     = "7_9-gen2"
+}
+
+variable "centos_8_sku" {
+  type        = string
+  description = "SKU for latest CentOS 8 "
+  default     = "8_5"
+}
+
+variable "centos_8_gen2_sku" {
+  type        = string
+  description = "SKU for latest CentOS 8 Gen2"
+  default     = "8_5-gen2"
+}
+
